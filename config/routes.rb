7ab4f0f234c3d4825c0b69d9d28bf65252071013
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-26 23:41:21 -0300

    ADDED collection route for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      get 'dashboard', to: 'posts#new'
+    end
+  end
+
+
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
