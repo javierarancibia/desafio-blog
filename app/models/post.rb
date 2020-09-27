a7a2a2b0d33b377210dbbd83a09f024c00b2eb77
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-27 15:50:37 -0300

    ORDER descendant for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
     validates :title, :content, :image_url, presence:true
+    validates :content, exclusion: { in: %w(spoiler), message: "%{value} is reserved." }
   
 end
